fd4e908d861e647c9c34b8645b8747d20fda2ce3
roseanfrankale <38437564+roseanfrankale@users.noreply.github.com>
2021-10-24 17:37:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,145 +1,278 @@
+(function () {
+  "use strict";
 
-  
-  //Get the button
-  
- let mybutton = document.getElementById("btn-back-to-top");
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 1000 ||
-    document.documentElement.scrollTop > 1000
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
 
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-  
-};
-
-
-// Lightbox TEST
-
-window.addEventListener('load', () => {
-  let portfolioContainer = select('.portfolio-container');
-  if (portfolioContainer) {
-    let portfolioIsotope = new Isotope(portfolioContainer, {
-      itemSelector: '.portfolio-item'
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
+  };
 
-    let portfolioFilters = select('#portfolio-flters li', true);
+  /**
+   * Back to top button
+   */
 
-    on('click', '#portfolio-flters li', function(e) {
-      e.preventDefault();
-      portfolioFilters.forEach(function(el) {
-        el.classList.remove('filter-active');
-      });
-      this.classList.add('filter-active');
+  //Get the button
+  let mybutton = document.getElementById("btn-back-to-top");
 
-      portfolioIsotope.arrange({
-        filter: this.getAttribute('data-filter')
-      });
-      portfolioIsotope.on('arrangeComplete', function() {
-        AOS.refresh()
-      });
-    }, true);
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 1000 ||
+      document.documentElement.scrollTop > 1000
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", backToTop);
+
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
 
-});
+  /**
+   * Mobile nav toggle
+   */
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
 
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on(
+    "click",
+    "#navbar .nav-link",
+    function (e) {
+      let section = select(this.hash);
+      if (section) {
+        e.preventDefault();
 
-const portfolioLightbox = GLightbox({
-  selector: '.portfolio-lightbox'
-});
+        let navbar = select("#navbar");
+        let header = select("#header");
+        let sections = select("section", true);
+        let navlinks = select("#navbar .nav-link", true);
 
-/**
- * Portfolio details slider
- */
-const swiper = new Swiper('.swiper',  {
-  // Optional parameters
-  spaceBetween: 30,
-  loop: true,
-  centeredSlides: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+        navlinks.forEach((item) => {
+          item.classList.remove("active");
+        });
 
+        this.classList.add("active");
 
-/**
- * Animate on Scroll Library
- */
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
 
-AOS.init();
+        if (this.hash == "#header") {
+          header.classList.remove("header-top");
+          sections.forEach((item) => {
+            item.classList.remove("section-show");
+          });
+          return;
+        }
 
-function () {
-  $('#portfolioCollapse1').on('shown.bs.collapse', function (e) {
-          $('html,body').animate({
-              scrollTop: $('#neubsis_card').offset().top
-          }, 100); 
-  }); 
-});
+        if (!header.classList.contains("header-top")) {
+          header.classList.add("header-top");
+          setTimeout(function () {
+            sections.forEach((item) => {
+              item.classList.remove("section-show");
+            });
+            section.classList.add("section-show");
+          }, 350);
+        } else {
+          sections.forEach((item) => {
+            item.classList.remove("section-show");
+          });
+          section.classList.add("section-show");
+        }
 
-function () {
-  $('#portfolioCollapse1,#card-close').on('hide.bs.collapse', function (e) {
-          $('html,body').animate({
-              scrollTop: $('#my-projects').offset().top
-          }, 100); 
-  }); 
-});
+        scrollto(this.hash);
+      }
+    },
+    true
+  );
 
-function () {
-  $('#portfolioCollapse2').on('shown.bs.collapse', function (e) {
-          $('html,body').animate({
-              scrollTop: $('#new_soil_card').offset().top 
-          }, 100); 
-  }); 
-});
+  /**
+   * Activate/show sections on load with hash links
+   */
+  window.addEventListener("load", () => {
+    if (window.location.hash) {
+      let initial_nav = select(window.location.hash);
 
-function () {
-  $('#portfolioCollapse2,#card-close').on('hide.bs.collapse', function (e) {
-          $('html,body').animate({
-              scrollTop: $('#my-projects').offset().top
-          }, 100); 
-  }); 
-});
+      if (initial_nav) {
+        let header = select("#header-top");
+        let navlinks = select("#navbar .nav-link", true);
 
+        header.classList.add("header-top");
 
-function () {
-  $('#portfolioCollapse3').on('shown.bs.collapse', function (e) {
-          $('html,body').animate({
-              scrollTop: $('#neubsis_card').offset().top
-          }, 1000); 
-  }); 
-});
+        navlinks.forEach((item) => {
+          if (item.getAttribute("href") == window.location.hash) {
+            item.classList.add("active");
+          } else {
+            item.classList.remove("active");
+          }
+        });
 
-function () {
-  $('#portfolioCollapse3,#card-close').on('hide.bs.collapse', function (e) {
-          $('html,body').animate({
-              scrollTop: $('#my-projects').offset().top
-          }, 100); 
-  }); 
-});
+        setTimeout(function () {
+          initial_nav.classList.add("section-show");
+        }, 350);
+
+        scrollto(window.location.hash);
+      }
+    }
+  });
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener("load", () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash);
+      }
+    }
+  });
+
+  /**
+   * Hero type effect
+   */
+  const typed = select(".typed");
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      showCursor: false,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
+
+  /**
+   * Preloader
+   */
+  let preloader = select("#preloader");
+  if (preloader) {
+    window.addEventListener("load", () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Card close transition
+   */
+  $(".close-icon").click(function () {
+    $("#neubsis_card", "new_soil_card").parent().fadeOut("slow");
+  });
+
+  $("#card-scroll1, a").on("click", function (e) {
+    $("html, body").animate(
+      {
+        scrollTop: $("#neubsis_card").offset().top,
+      },
+      "100"
+    );
+    e.preventDefault();
+  });
+
+  $("#card-scroll2, a").on("click", function (e) {
+    $("html, body").animate(
+      {
+        scrollTop: $("#new_soil_card").offset().top,
+      },
+      ""
+    );
+    e.preventDefault();
+  });
+
+  $("#card-scroll3, a").on("click", function (e) {
+    $("html, body").animate(
+      {
+        scrollTop: $("#bruvue_card").offset().top,
+      },
+      "300"
+    );
+    e.preventDefault();
+  });
+
+  /* Masonry
+   * ---------------------------------------------------- */
+
+  var masonryFolio = function () {
+    var containerBricks = $(".masonry");
+
+    containerBricks.imagesLoaded(function () {
+      containerBricks.masonry({
+        itemSelector: ".masonry__brick",
+        resize: true,
+      });
+    });
+  };
+
+  new Swiper('.swiper', {
+    // Optional parameters
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+})();
 
 
 
