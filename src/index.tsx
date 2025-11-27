a543cd5e5295870b99d5c26fbdc0ddd5a2f60421
Rosean Alexander <rosean.frankalexander@gmail.com>
2025-11-27 16:37:23 +0100

    deleting old files

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.css'; // This applies your global styles
 
+// Find the element with id "root" in your HTML
 const rootElement = document.getElementById('root');
+
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   );
+} else {
+  console.error("Failed to find the root element. Make sure index.html has a <div id='root'></div>");
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/[https://github.com/roseanfrankale/roseanfrankale.github.io',
   plugins: [react()]
 })
